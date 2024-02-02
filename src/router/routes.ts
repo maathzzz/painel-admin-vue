7a5d1b8e7d7099b1dff8256340068a151531eedd
Matheus A. Favero <matheusdeamorim20@gmail.com>
2024-02-02 05:50:33 -0300

    fix: type of vue-router params

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,7 @@
 import { useAuth } from "../stores/auth.ts";
+import { RouteLocationNormalized, NavigationGuardNext } from "vue-router";
 
-export default async function routes(to, from, next) {
+export default async function routes(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) {
 	const auth = useAuth();
 
 	if (to.meta?.auth) {
